ee3d542ac8031416ccda2f8e61f96985f3d78837
Arjun Mavai <yuyureka212@gmail.com>
2023-09-01 22:12:38 +0530

    after adding animation to all section of header

--- script.js
+++ script.js
@@ -1,100 +1,76 @@
 console.log("script file is running properly");
-let isNavbarActive = false;
-
-
-document.addEventListener('DOMContentLoaded', function () {
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
- menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-    isNavbarActive = !isNavbarActive;
-
-}
-
  
-// typed output code
 
- 
- 
-    
+document.addEventListener("DOMContentLoaded", function () {
+  // toggle icon navbar
+  let menuIcon = document.querySelector("#menu-icon");
+  let navbar = document.querySelector(".navbar");
 
-// scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+  };
 
-window.onscroll = () => {
+  // typed output code
+
+  // scroll sections
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll("header nav a");
+
+  window.onscroll = () => {
     console.log(sections);
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset =  sec.offsetTop-100;
-        let height  = sec.offsetHeight;
-        let id = sec.getAttribute('id')
+    sections.forEach((sec) => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 100;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute("id");
 
-        if(sec){
-            if(top >= offset && top < offset + height){
-                // active navbar links
-                navLinks.forEach(links=>{
-                    links.classList.remove('active');
-                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-                });
-                // active sections for animation on scroll
-                sec.classList.add('show-animate');
-            }
-            // if want to use animation that repeats on scroll use this 
-            else {
-                sec.classList.remove('show-animate')
-            }
+      if (sec) {
+        if (top >= offset && top < offset + height) {
+          // active navbar links
+          navLinks.forEach((links) => {
+            links.classList.remove("active");
+            document
+              .querySelector("header nav a[href*=" + id + "]")
+              .classList.add("active");
+          });
+          // active sections for animation on scroll
+          sec.classList.add("show-animate");
         }
-    })
-
-
-   
-
-
-window.onscroll = () => {
-    let header = document.getElementById('header');
-
-    if(header){
-
-        if(!isNavbarActive){
-            header.classList.toggle('sticky' , window.scrollY > 100)
-        
+        // if want to use animation that repeats on scroll use this
+        else {
+          sec.classList.remove("show-animate");
         }
-        else{
-            header.classList.remove('sticky');
-        }
-    }
-
-    
-}
+      }
+    });
     // sticky header
-
+    let header = document.querySelector("header");
+    if (header) {
+      header.classList.toggle("sticky ", window.scrollY > 100);
+    }
 
     // remove toggle icon and navbar when click navbar links (scroll)
 
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active')
-
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
 
     // animation footer on scroll
 
+    let footer = document.querySelector('footer');
+if (footer) {
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
 
+  };
 });
 
 console.log("script file is running properly end of file");
 
-
-
-
 // if(top >= offset && top < offset + height){:
 //     What it does: Checks if the user has scrolled to the current section.
 //     Analogy: You're in an elevator and you're checking if you've reached the correct floor.
-//     DOM Example: offset is like the floor number, height is the height of that floor, 
+//     DOM Example: offset is like the floor number, height is the height of that floor,
 // and top is where the elevator currently is.
-//     Grid Example: Imagine a grid where each cell is a floor. 
-// offset is the starting point of a cell, 
+//     Grid Example: Imagine a grid where each cell is a floor.
+// offset is the starting point of a cell,
 // and offset + height is the ending point. You're checking if you're within that cell.
